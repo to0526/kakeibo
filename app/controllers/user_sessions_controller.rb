73f42885d41cfb50d672ad9e35319041e1780d58
ohtsuka <t.o.0526@gmail.com>
2020-07-17 23:42:12 +0900

    日本語化、不要なファイル削除

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,15 +5,15 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to(:users, notice: 'Login successful')
+      redirect_back_or_to(:users, notice: 'ログインに成功しました')
     else
-      flash.now[:alert] = 'Login failed'
+      flash.now[:alert] = 'ログインに失敗しました'
       render action: 'new'
     end
   end
 
   def destroy
     logout
-    redirect_to(:users, notice: 'Logged out!')
+    redirect_to(:users, notice: 'ログアウトしました')
   end
 end
