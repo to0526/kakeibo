1fc717eecb94561b9d31a8c08b9c0394cc2ace60
ohtsuka <t.o.0526@gmail.com>
2020-07-17 23:09:44 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: "users#index"
   resources :payment_classifications
   resources :payment_methods
   resources :users
+  resources :user_sessions, only: [:create]
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
