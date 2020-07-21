bd0ca48af64e08b2a3725c86eac498ae2d1bd243
ohtsuka <t.o.0526@gmail.com>
2020-07-21 20:12:44 +0900

    支払い分類、支払金額のseedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,23 @@ unless User.exists?(email: "test2@example.com")
     name: "テスト２"
   )
 end
+
+PaymentClassification.find_or_create_by!(purpose: :required, name: "居住費")
+PaymentClassification.find_or_create_by!(purpose: :required, name: "水道光熱費")
+PaymentClassification.find_or_create_by!(purpose: :required, name: "通信費")
+PaymentClassification.find_or_create_by!(purpose: :required, name: "食費")
+PaymentClassification.find_or_create_by!(purpose: :required, name: "雑費・医療費")
+PaymentClassification.find_or_create_by!(purpose: :required, name: "一般保険料")
+PaymentClassification.find_or_create_by!(purpose: :enjoyment, name: "娯楽費")
+PaymentClassification.find_or_create_by!(purpose: :enjoyment, name: "コンビニで買ったおやつ")
+PaymentClassification.find_or_create_by!(purpose: :enjoyment, name: "本、雑誌")
+PaymentClassification.find_or_create_by!(purpose: :enjoyment, name: "服")
+PaymentClassification.find_or_create_by!(purpose: :enjoyment, name: "DVD")
+PaymentClassification.find_or_create_by!(purpose: :enjoyment, name: "使途不明金")
+
+PaymentMethod.find_or_create_by!(name: "現金")
+PaymentMethod.find_or_create_by!(name: "ID")
+PaymentMethod.find_or_create_by!(name: "クレジットカード(楽天)")
+PaymentMethod.find_or_create_by!(name: "クレジットカード(SEIYU)")
+PaymentMethod.find_or_create_by!(name: "クレジットカード(ヨドバシ)")
+PaymentMethod.find_or_create_by!(name: "クレジットカード(VISA)")
