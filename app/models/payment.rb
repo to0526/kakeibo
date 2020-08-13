619c150457851f9e457ca94d070628ef337dc29f
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-08-13 20:27:09 +0900

    支払はマイナスにする

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,9 @@
 class Payment < Item
+  before_save :make_negative_amount, if: Proc.new { |payment| payment.amount > 0 }
+
+  private
+
+  def make_negative_amount
+    self.amount = amount * -1
+  end
 end
