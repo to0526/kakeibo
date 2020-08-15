ad82b062a5ffd4f2ec5abc1b8d668e5f7e246000
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-08-15 17:51:48 +0900

    収支方法のテスト追加

--- app/models/payment_classification.rb
+++ app/models/payment_classification.rb
@@ -2,5 +2,5 @@ class PaymentClassification < ApplicationRecord
   enum purpose: { required: 0, enjoyment: 1 }
   validates :name, presence: true
   validates :sort, presence: true
-  has_many :payments, dependent: :restrict_with_error
+  has_many :items, dependent: :restrict_with_error
 end
--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,4 +1,4 @@
 class PaymentMethod < ApplicationRecord
   validates :name, presence: true
-  has_many :payments, dependent: :restrict_with_error
+  has_many :items, dependent: :restrict_with_error
 end
