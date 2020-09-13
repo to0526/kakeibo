3164e06b0d5c3f02476e7e9167e2e2e0439ddf6b
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-13 09:01:29 +0900

    支払い分類の円グラフ表示

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,12 @@
 class Payment < Item
   before_save :make_negative_amount, if: Proc.new { |payment| payment.amount > 0 }
 
+  def self.name_with_amount(date:)
+    monthly(date).group(:payment_classification_id).sum(:amount).each_with_object({}) do |(id, amount), hash|
+      hash[PaymentClassification.find(id).name] = amount
+    end
+  end
+
   private
 
   def make_negative_amount
