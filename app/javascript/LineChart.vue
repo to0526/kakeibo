a677ea52257d4d3c987afc5ec8a8eb4e893bc377
ohtsuka takahiro <t.o.0526@gmail.com>
2020-10-18 11:25:04 +0900

    LineChart

---
+++ app/javascript/LineChart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Line } from "vue-chartjs"
+
+export default {
+  extends: Line,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
+<style>
+</style>
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,13 +1,44 @@
 <template>
-  <BarChart></BarChart>
+  <LineChart v-bind:chartdata="chartdata"></LineChart>
 </template>
 
 <script>
-import BarChart from "./BarChart"
+import LineChart from "./LineChart"
+
+const dates = [...Array(32).keys()]
+dates.shift()
+
+const selectable_months = ["2020/07", "2020/08", "2020/09", "2020/10"]
+const disabled_months = [
+  {
+    label: '2020/07',
+    data: [100,200,300]
+  },
+  {
+    label: '2020/08',
+    data: [0,100,200,300]
+  },
+  {
+    label: '2020/09',
+    data: [0,0,100,200,300]
+  },
+  {
+    label: '2020/10',
+    data: [0,0,0,100,200,300]
+  }
+]
 
 export default {
   components: {
-    BarChart
+    LineChart
+  },
+  data() {
+    return {
+      chartdata: {
+        labels: dates,
+        datasets: disabled_months,
+      }
+    }
   }
 }
 </script>
