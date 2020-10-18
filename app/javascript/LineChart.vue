9959d10f516ac0417123c437176e3cbad6139fce
ohtsuka takahiro <t.o.0526@gmail.com>
2020-10-18 17:55:59 +0900

    複数月の表示非表示を切り替えられる

--- app/javascript/LineChart.vue
+++ app/javascript/LineChart.vue
@@ -1,10 +1,12 @@
 <script>
-import { Line } from "vue-chartjs"
+import { Line, mixins } from "vue-chartjs"
+const { reactiveProp } = mixins
 
 export default {
   extends: Line,
+  mixins: [reactiveProp],
   props: {
-    chartdata: {
+    chartData: {
       type: Object,
       default: null
     }
@@ -18,7 +20,7 @@ export default {
     }
   },
   mounted() {
-    this.renderChart(this.chartdata, this.options)
+    this.renderChart(this.chartData, this.options)
   }
 }
 </script>
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,32 +1,22 @@
 <template>
-  <LineChart v-bind:chartdata="chartdata"></LineChart>
+  <div>
+    <input type="checkbox" id="2020/07" v-on:click="toggle('2020/07')">
+      <label for="2020/07">2020/07</label>
+    </input>
+    <input type="checkbox" id="2020/08" v-on:click="toggle('2020/08')">
+      <label for="2020/08">2020/08</label>
+    </input>
+    <input type="checkbox" id="2020/09" v-on:click="toggle('2020/09')">
+      <label for="2020/09">2020/09</label>
+    </input>
+    <LineChart v-bind:chartData="datacollection"></LineChart>
+  </div>
 </template>
 
 <script>
 import LineChart from "./LineChart"
 
-const dates = [...Array(32).keys()]
-dates.shift()
-
-const selectable_months = ["2020/07", "2020/08", "2020/09", "2020/10"]
-const disabled_months = [
-  {
-    label: '2020/07',
-    data: [100,200,300]
-  },
-  {
-    label: '2020/08',
-    data: [0,100,200,300]
-  },
-  {
-    label: '2020/09',
-    data: [0,0,100,200,300]
-  },
-  {
-    label: '2020/10',
-    data: [0,0,0,100,200,300]
-  }
-]
+const dates = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 
 export default {
   components: {
@@ -34,9 +24,44 @@ export default {
   },
   data() {
     return {
-      chartdata: {
+      datacollection: {
         labels: dates,
-        datasets: disabled_months,
+        datasets: null,
+      },
+      dates: dates,
+      foo: [
+        {
+          label: '2020/07',
+          data: [100,200,300],
+        },
+        {
+          label: '2020/08',
+          data: [0,100,200,300],
+        },
+        {
+          label: '2020/09',
+          data: [0,0,100,200,300],
+        }
+      ],
+    }
+  },
+  methods: {
+    toggle(month) {
+      const datasets = this.datacollection.datasets
+      const dataset = datasets.find(x => x.label === month)
+      let new_datasets = []
+      const new_dataset = this.foo.find(x => x.label === month)
+
+      if (dataset === undefined) {
+        new_datasets = datasets.concat()
+        new_datasets.push(new_dataset)
+      } else {
+        new_datasets = datasets.filter(x => x !== new_dataset)
+      }
+
+      this.datacollection = {
+        labels: this.dates,
+        datasets: new_datasets,
       }
     }
   }
