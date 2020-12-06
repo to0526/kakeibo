c1df69edcf18e527f9f09cf4e85a4e2e3fc008f5
ohtsuka takahiro <t.o.0526@gmail.com>
2020-12-06 14:38:07 +0900

    配列生成リファクタ

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -18,7 +18,10 @@
 <script>
 import LineChart from "./LineChart"
 
-const dates = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+let dates = []
+for (let i = 1; i <= 31; i++) {
+  dates.push(i)
+}
 
 export default {
   components: {
