8fdee660cfa92056ebb86b85e71d36151f69faea
ohtsuka takahiro <t.o.0526@gmail.com>
2021-02-21 01:09:48 +0900

    不要な記述削除

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -20,11 +20,6 @@
 import LineChart from "./LineChart"
 import gql from "graphql-tag"
 
-// let dates = []
-// for (let i = 1; i <= 31; i++) {
-//   dates.push(i)
-// }
-
 export default {
   components: {
     LineChart
@@ -34,9 +29,6 @@ export default {
       datacollection: {},
       selectableYearMonths: [],
       selectedYearMonths: [],
-      // dates: dates,
-      // all_datasets: [],
-      // loaded: false,
     }
   },
   methods: {
@@ -47,35 +39,8 @@ export default {
         this.selectedYearMonths.push(year_month)
       }
       this.$apollo.queries.datacollection.refetch()
-      // const displayed_datasets = this.datacollection.datasets
-      // const checked_dataset = this.all_datasets.find(x => x.label === year_month)
-      // let new_datasets = []
-
-      // if (displayed_datasets.includes(checked_dataset)) {
-      //   new_datasets = displayed_datasets.filter(x => x !== checked_dataset)
-      // } else {
-      //   new_datasets = displayed_datasets.concat()
-      //   new_datasets.push(checked_dataset)
-      // }
-
-      // this.datacollection = {
-      //   labels: this.dates,
-      //   datasets: new_datasets,
-      // }
     }
   },
-  // computed: {
-  //   year_months: function() {
-  //     return this.all_datasets.map(x => x.label)
-  //   }
-  // },
-  // created() {
-  //   fetch("/api/payments")
-  //     .then(response => response.json())
-  //     .then(data => this.all_datasets = data)
-  //     .catch(e => alert(e))
-  //   this.loaded = true
-  // },
   apollo: {
     datacollection: {
       query: gql`query($labels: [String]!) {
