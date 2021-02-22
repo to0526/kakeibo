14955f00c4489a42d0f6f12633265ba09cc4b43d
ohtsuka takahiro <t.o.0526@gmail.com>
2021-02-22 22:36:27 +0900

    不要な行を削除

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,23 +7,6 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
-
-    field :line_chart_items, [LineChart::ItemType], null: true do
-      argument :labels, [String], required: true
-    end
-
-    def line_chart_items(labels:)
-      labels.map do |label|
-        ::LineChart::Item.new(label: label).to_json
-      end
-    end
-
     field :datacollection, DatacollectionType, null: true do
       argument :labels, [String, null: true], required: true
     end
