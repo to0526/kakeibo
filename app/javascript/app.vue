24cbf61d851019b144930aa0f5e8dd6d2dc7fd94
ohtsuka takahiro <t.o.0526@gmail.com>
2021-02-24 22:57:31 +0900

    tableに色を表示

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -16,14 +16,23 @@
       <b-table-column field="yearMonth" label="年月" v-slot="props">
         {{ props.row.yearMonth }}
       </b-table-column>
+
       <b-table-column field="income" label="収入" numeric v-slot="props">
-        {{ props.row.income }}
+        <span :class="type(props.row.income)">
+          {{ props.row.income }}
+        </span>
       </b-table-column>
+
       <b-table-column field="payment" label="支出" numeric v-slot="props">
-        {{ props.row.payment }}
+        <span :class="type(props.row.payment)">
+          {{ props.row.payment }}
+        </span>
       </b-table-column>
+
       <b-table-column field="total" label="合計" numeric v-slot="props">
-        {{ props.row.total }}
+        <span :class="type(props.row.total)">
+          {{ props.row.total }}
+        </span>
       </b-table-column>
     </b-table>
   </div>
@@ -82,6 +91,15 @@ export default {
         return {
           labels: [...this.selectedYearMonths].sort()
         }
+      }
+    }
+  },
+  methods: {
+    type: function(value) {
+      if (value.slice(0, 1) == "-") {
+        return 'has-text-danger'
+      } else {
+        return 'has-text-info'
       }
     }
   }
