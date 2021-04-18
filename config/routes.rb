7344c714b2fe3158ded06f06fa52bfd7c9fcd783
ohtsuka takahiro <t.o.0526@gmail.com>
2021-04-19 05:51:08 +0900

    docker-compose exec app rails g controller Api::UsersController index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'users/index'
+  end
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
