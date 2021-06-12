d4aa142cffee1e566d39410962da0b55fe72f3fe
ohtsuka takahiro <t.o.0526@gmail.com>
2021-06-13 07:42:14 +0900

    不要行削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    payed_on_where_args = ""
     if params[:year_months].present?
       @items = Item.where_year_months(params[:year_months]).order(payed_on: :desc)
     else
