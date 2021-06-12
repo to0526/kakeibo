af891aa0dbc53f87a5cb2c6034edae634b3ed2ed
ohtsuka takahiro <t.o.0526@gmail.com>
2021-06-13 07:25:27 +0900

    年月で検索

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,13 @@
 class ItemsController < ApplicationController
   def index
-    if params[:q].nil?
-      params[:q] = {}
-      params[:q][:payed_on_gteq] = Date.today.beginning_of_month.to_s
-      params[:q][:payed_on_lteq] = Date.today.end_of_month.to_s
-    elsif params[:q][:payed_on_gteq].present?
-      params[:q][:payed_on_lteq] = params[:q][:payed_on_gteq].to_date.end_of_month.to_s
+    payed_on_where_args = ""
+    if params[:year_months].present?
+      payed_on_where_args = params[:year_months].map do |year_month|
+        from = Date.parse(year_month).beginning_of_month
+        to   = Date.parse(year_month).end_of_month
+        "(payed_on BETWEEN '#{from}' AND '#{to}')"
+      end.join(" OR ")
     end
-    @q = Item.ransack(params[:q])
-    @items = @q.result(distinct: true).order(payed_on: :desc)
-    @payments_total_amount = @items.where(type: "Payment").map { |item| item.amount }.sum
-    @incomes_total_amount = @items.where(type: "Income").map { |item| item.amount }.sum
-    @items_amount_with_classification =
-      PaymentClassification.all.order(:sort).each_with_object([]) do |payment_classification, arr|
-        arr << {
-          payment_classification: payment_classification.name,
-          amount: @items.where(payment_classification_id: payment_classification.id).map{|item|item.amount}.sum.to_s(:delimited)
-        }
-      end
-    @items_amount_with_classification = @items_amount_with_classification.in_groups(2, false)
+    @items = Item.where(payed_on_where_args).order(payed_on: :desc)
   end
 end
