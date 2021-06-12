c5fe72d6f0c939d54d15d0ee929402384a0db160
ohtsuka takahiro <t.o.0526@gmail.com>
2021-06-13 07:37:43 +0900

    年月の検索をscope化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,9 @@ class ItemsController < ApplicationController
   def index
     payed_on_where_args = ""
     if params[:year_months].present?
-      payed_on_where_args = params[:year_months].map do |year_month|
-        from = Date.parse(year_month).beginning_of_month
-        to   = Date.parse(year_month).end_of_month
-        "(payed_on BETWEEN '#{from}' AND '#{to}')"
-      end.join(" OR ")
+      @items = Item.where_year_months(params[:year_months]).order(payed_on: :desc)
+    else
+      @items = Item.none
     end
-    @items = Item.where(payed_on_where_args).order(payed_on: :desc)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,15 @@
 class Item < ApplicationRecord
   scope :monthly, -> (date) { where(payed_on: date.beginning_of_month..date.end_of_month) }
 
+  scope :where_year_months, -> (year_months) do
+    where_args = year_months.map do |year_month|
+      from = Date.parse(year_month).beginning_of_month
+      to   = Date.parse(year_month).end_of_month
+      "(payed_on BETWEEN '#{from}' AND '#{to}')"
+    end.join(" OR ")
+    where(where_args)
+  end
+
   belongs_to :user
   belongs_to :payment_classification
   belongs_to :payment_method
