f970c98f424328de46bbb7658207a29d70487c28
ohtsuka takahiro <t.o.0526@gmail.com>
2021-06-16 23:50:18 +0900

    displayed_amount

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     dates = dates.select { |d| d.day == 1 }
     dates.map{|d| d.strftime("%Y/%m")}.reverse
   end
+
+  def displayed_amount(amount)
+    if amount < 0
+      tag.td amount.to_s(:delimited), align: "right", class: "red"
+    else
+      tag.td amount.to_s(:delimited), align: "right", class: "blue"
+    end
+  end
 end
