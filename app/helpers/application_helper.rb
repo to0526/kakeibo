bf5b2808441270159c255fb09678804bfe236c95
ohtsuka takahiro <t.o.0526@gmail.com>
2021-06-26 10:40:01 +0900

    収支一覧で翌月分も表示する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def year_months
-    dates = (Date.new(2020,07,01)..Date.today.beginning_of_month).to_a
+    dates = (Date.new(2020,07,01)..Date.today.next_month.beginning_of_month).to_a
     dates = dates.select { |d| d.day == 1 }
     dates.map{|d| d.strftime("%Y/%m")}.reverse
   end
